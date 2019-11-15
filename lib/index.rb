4856a71a61563a2a936d7affccf848dfb9c001d2
Christopher Pezza <pezza.chris@gmail.com>
2019-11-14 23:18:06 -0500

    Add param usage to haml lint cmd

--- lib/index.rb
+++ lib/index.rb
@@ -18,11 +18,26 @@ end
   owner: ENV["GITHUB_REPOSITORY_OWNER"] || @event_json.dig("repository", "owner", "login"),
   repo: ENV["GITHUB_REPOSITORY_NAME"] || @event_json.dig("repository", "name"),
 }
+
+@params = {
+  file_paths: ENV["FILE_PATHS"],
+  config_path: ENV["CONFIG_PATH"],
+  exclude_files: ENV["EXCLUDE_FILES"],
+  fail_level: ENV["FAIL_LEVEL"]
+}
+
+@haml = "haml-lint -r json"
+@haml += " -c " + @params.config_path if ENV["CONFIG_PATH"]
+@haml += " -e " + @params.exclude_files if ENV["EXCLUDE_FILES"]
+@haml += " --fail-level " + @params.fail_level
+
 @report =
   if ENV["REPORT_PATH"]
     read_json(ENV["REPORT_PATH"])
   else
-    Dir.chdir(ENV["GITHUB_WORKSPACE"]) { JSON.parse(`haml-lint -r json`) }
+    Dir.chdir(ENV["GITHUB_WORKSPACE"]) {
+      JSON.parse(System(@haml))
+    }
   end
 
 GithubCheckRunService.new(@report, @github_data, ReportAdapter).run
