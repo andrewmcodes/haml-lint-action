b34ddf18653690d2fc54b679221944b30550b47e
Christopher Pezza <pezza.chris@gmail.com>
2019-11-14 22:58:25 -0500

    Add version support

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -2,6 +2,11 @@
 
 set -e
 
-gem install haml-lint ${INPUT_ADDITIONAL_GEMS}
+if ["${INPUT_VERSION}" != ""]
+then
+  gem install haml-lint ${INPUT_ADDITIONAL_GEMS}
+else
+  gem install haml-lint -v ${INPUT_VERSION} ${INPUT_ADDITIONAL_GEMS}
+fi
 
 ruby /action/lib/index.rb
