f977668a2d5de321407db4be8c2d9cfa581f4458
Andrew Mason <andrewmcodes@protonmail.com>
2019-11-18 19:59:46 -0500

    fix lints

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-if [ "${INPUT_BUNDLE}" == "true" ]
+if [ "${INPUT_BUNDLE}" = "true" ]
 then
   bundle install --path vendor/bundle --jobs 4 --retry 3
 elif [ -z "${INPUT_VERSION}" ]
 then
-  gem install haml-lint ${INPUT_ADDITIONAL_GEMS}
+  gem install haml-lint "${INPUT_ADDITIONAL_GEMS}"
 else
-  gem install haml-lint -v ${INPUT_VERSION} ${INPUT_ADDITIONAL_GEMS}
+  gem install haml-lint -v "${INPUT_VERSION} ${INPUT_ADDITIONAL_GEMS}"
 fi
 
 ruby /action/lib/index.rb
