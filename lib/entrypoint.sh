02139c328a985a0ae4ceec5eed8899cbfd1a3279
Andrew Mason <andrewmcodes@protonmail.com>
2019-11-18 20:07:19 -0500

    extract additional gems install

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -7,9 +7,14 @@ then
   bundle install --path vendor/bundle --jobs 4 --retry 3
 elif [ -z "${INPUT_VERSION}" ]
 then
-  gem install haml-lint "${INPUT_ADDITIONAL_GEMS}"
+  gem install haml-lint
 else
-  gem install haml-lint -v "${INPUT_VERSION}" "${INPUT_ADDITIONAL_GEMS}"
+  gem install haml-lint -v "${INPUT_VERSION}"
+fi
+
+if [ -n "${INPUT_ADDITIONAL_GEMS}" ]
+then
+  gem install "${INPUT_ADDITIONAL_GEMS}"
 fi
 
 ruby /action/lib/index.rb
