18fbd89fbab68ed727706b1ced5fdcf563a69013
Andrew Mason <andrewmcodes@protonmail.com>
2019-11-18 19:40:08 -0500

    fix syntax errors pointed out in andrewmcodes/rubocop-linter-action#55

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-if ["${INPUT_BUNDLE}" == "true"]
+if [ "${INPUT_BUNDLE}" == "true" ]
 then
-  bundle install --jobs 4 --retry 3
-elif ["${INPUT_VERSION}" != ""]
+  bundle install --path vendor/bundle --jobs 4 --retry 3
+elif [ -z "${INPUT_VERSION}" ]
 then
   gem install haml-lint ${INPUT_ADDITIONAL_GEMS}
 else
