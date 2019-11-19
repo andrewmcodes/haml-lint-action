600aa9f753b4e1cef9e4f478451db767ee43ad94
Andrew Mason <andrewmcodes@protonmail.com>
2019-11-18 20:01:18 -0500

    fix error

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -9,7 +9,7 @@ elif [ -z "${INPUT_VERSION}" ]
 then
   gem install haml-lint "${INPUT_ADDITIONAL_GEMS}"
 else
-  gem install haml-lint -v "${INPUT_VERSION} ${INPUT_ADDITIONAL_GEMS}"
+  gem install haml-lint -v "${INPUT_VERSION}" "${INPUT_ADDITIONAL_GEMS}"
 fi
 
 ruby /action/lib/index.rb
