c936374f55ee599b19090a3d8159f9242d12c04b
Andrew Mason <andrewmcodes@protonmail.com>
2019-11-21 22:21:34 -0500

    fix mispelled gem name

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -5,9 +5,9 @@ set -e
 if [ "${INPUT_BUNDLE}" = "true" ]; then
   bundle install --deployment --jobs 4 --retry 3
 elif [ -z "${INPUT_VERSION}" ]; then
-  gem install haml-lint
+  gem install haml_lint
 else
-  gem install haml-lint -v "${INPUT_VERSION}"
+  gem install haml_lint -v "${INPUT_VERSION}"
 fi
 
 if [ -n "${INPUT_ADDITIONAL_GEMS}" ]; then
